0997f7df02c69f054187bc7db56a3568abdd14e6
Jeff Oryen <jeffor@zillowgroup.com>
2024-03-16 00:09:02 -0700

    Allow hidden files

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,26 +1,26 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope-fzf-native.nvim",
-  },
-  opts = {
-    defaults = {
-      layout_strategy = "vertical",
-      layout_config = {
-        height = 0.95,
-        prompt_position = "top",
-        vertical = {
-          mirror = true,
-          preview_cutoff = 40,
-        },
-      },
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-fzf-native.nvim",
     },
-  },
-  config = function(_, opts)
-    pcall(require("telescope").load_extension, "fzf")
-    require("telescope").setup(opts)
-  end,
+    opts = {
+        defaults = {
+            layout_strategy = "vertical",
+            layout_config = {
+                height = 0.95,
+                prompt_position = "top",
+                vertical = {
+                    mirror = true,
+                    preview_cutoff = 40,
+                },
+            },
+        },
+    },
+    config = function(_, opts)
+        pcall(require("telescope").load_extension, "fzf")
+        require("telescope").setup(opts)
+    end,
     -- stylua: ignore
 		keys = {
 			{ '<leader>?',       function() require('telescope.builtin').oldfiles() end, desc = '[?] Find recently opened files' },
@@ -37,7 +37,7 @@ return {
 				desc = '[/] Fuzzily search in current buffer'
 			},
 			{ '<leader>gf', function() require('telescope.builtin').git_files() end,   desc = 'Search [G]it [F]iles' },
-			{ '<c-p>',      function() require('telescope.builtin').find_files() end,  desc = '[S]earch [F]iles' },
+			{ '<c-p>',      function() require('telescope.builtin').find_files({hidden = true}) end,  desc = '[S]earch [F]iles' },
 			{ '<leader>sh', function() require('telescope.builtin').help_tags() end,   desc = '[S]earch [H]elp' },
 			{ '<leader>sw', function() require('telescope.builtin').grep_string() end, desc = '[S]earch current [W]ord' },
 			{ '<leader>sg', function() require('telescope.builtin').live_grep() end,   desc = '[S]earch by [G]rep' },
