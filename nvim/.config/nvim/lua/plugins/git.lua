8b730e2d1846eb1b7765b0c7b2b1282fa0d53e67
Jeff Oryen <jeffor@zillowgroup.com>
2024-03-16 00:07:45 -0700

    Neogit + Diffview

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -1,19 +1,19 @@
--- return {
---   "tpope/vim-fugitive",
---   cmd = "Git",
---   dependencies = { "shumphrey/fugitive-gitlab.vim" },
--- }
-
 return {
-    "NeogitOrg/neogit",
-    dependencies = {
-        "nvim-lua/plenary.nvim", -- required
-        "sindrets/diffview.nvim", -- optional - Diff integration
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim", -- required
+            "sindrets/diffview.nvim", -- optional - Diff integration
 
-        -- Only one of these is needed, not both.
-        "nvim-telescope/telescope.nvim", -- optional
-        -- "ibhagwan/fzf-lua",              -- optional
+            -- Only one of these is needed, not both.
+            "nvim-telescope/telescope.nvim", -- optional
+            -- "ibhagwan/fzf-lua",              -- optional
+        },
+        cmd = "Neogit",
+        config = true,
     },
-    cmd = "Neogit",
-    config = true,
+    {
+        "sindrets/diffview.nvim",
+        cmd = "Diffview",
+    },
 }
