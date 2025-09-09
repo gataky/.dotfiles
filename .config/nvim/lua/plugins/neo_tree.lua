d44095c11de1210e9f7438b16e21b36f3158dec4
Jeff Oryen <jeffor@zillowgroup.com>
2025-09-09 11:57:01 -0700

    update neo-tree config

--- .config/nvim/lua/plugins/neo_tree.lua
+++ .config/nvim/lua/plugins/neo_tree.lua
@@ -9,17 +9,31 @@
 -- end)
 
 add({
-  source = 'nvim-neo-tree/neo-tree.nvim',
-  checkout = 'v3.x',
-  depends = {
-    "nvim-lua/plenary.nvim",
-    "MunifTanjim/nui.nvim",
-    "nvim-tree/nvim-web-devicons", -- optional, but recommended
-  }
+    source = 'nvim-neo-tree/neo-tree.nvim',
+    checkout = 'v3.x',
+    depends = {
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        "nvim-tree/nvim-web-devicons", -- optional, but recommended
+    }
 })
 
 later(function()
-    require('neo-tree').setup({})
+    require('neo-tree').setup({
+        filesystem = {
+            filtered_items = {
+                hide_dotfiles = false,
+            }
+        },
+        default_component_configs = {
+            indent = {
+                with_expanders = true,
+                expander_collapsed = "",
+                expander_expanded = "",
+                expander_highlight = "NeoTreeExpander",
+            },
+        },
+    })
 
     vim.api.nvim_set_keymap('n', '<leader>e', ':Neotree toggle<cr>', { noremap = true, expr = false })
 end)
