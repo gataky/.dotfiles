bb768d7ef75bb2d0734b40f35757cb26aa65c51b
Jeff Oryen <jeffor@zillowgroup.com>
2025-09-08 19:33:39 -0700

    add themes for neovim

---
+++ .config/nvim/lua/themes.lua
@@ -0,0 +1,10 @@
+-- Theme configurations
+-- This file handles all color schemes and visual themes
+
+-- Load theme configurations
+require('plugins.gruvbox_material')
+require('plugins.kanagawa')
+
+-- Set the active colorscheme
+-- Change this to switch themes: 'gruvbox-material', 'kanagawa', etc.
+vim.cmd('colorscheme gruvbox-material')
