d8ad574f07e07899f2a38867a15e96e003e8b802
jeff <jeff@foo.bar>
2026-08-11 15:45:40 -0700

    feat: keymap for notify history

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -2,3 +2,8 @@ vim.keymap.set("i", "jk", "<esc>", { noremap = true })
 vim.keymap.set("n", "<esc>", ":noh<cr>", { noremap = true })
 
 vim.keymap.set("v", "gy", '"+y', { noremap = true })
+
+
+vim.keymap.set('n', '<leader>nh', function()
+  MiniNotify.show_history()
+end, { desc = 'Show notification history' })
--- nvim/.config/nvim/lua/editing.lua
+++ nvim/.config/nvim/lua/editing.lua
@@ -1,14 +1,26 @@
-later(function() require('mini.align').setup() end)
-later(function() require('mini.comment').setup() end)
-later(function() require('mini.jump').setup() end)
-later(function() require('mini.jump2d').setup() end)
-later(function() require('mini.pairs').setup() end)
-later(function() require('mini.surround').setup() end)
+later(function()
+    require('mini.align').setup()
+end)
+later(function()
+    require('mini.comment').setup()
+end)
+later(function()
+    require('mini.jump').setup()
+end)
+later(function()
+    require('mini.jump2d').setup()
+end)
+later(function()
+    require('mini.pairs').setup()
+end)
+later(function()
+    require('mini.surround').setup()
+end)
 
 now(function()
     require('mini.basics').setup({
         mappings = {
-            basic = false,   -- Disable basic mappings
+            basic = false, -- Disable basic mappings
             option_toggle_prefix = [[\]],
             windows = false, -- Disable window mappings
             move_with_alt = false,
@@ -22,14 +34,14 @@ later(function()
     vim.api.nvim_set_keymap('n', '<c-c>', '', {
         callback = function()
             require('mini.bufremove').wipeout()
-        end
+        end,
     })
 end)
 
 later(function()
     require('mini.trailspace').setup()
-    vim.api.nvim_create_autocmd("BufWritePre", {
-        pattern = "*",
+    vim.api.nvim_create_autocmd('BufWritePre', {
+        pattern = '*',
         callback = function()
             local trailspace = require('mini.trailspace')
             trailspace.trim()
